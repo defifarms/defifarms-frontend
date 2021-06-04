f87d8aaef916d66e76bc371453031200fc3841fa
join-tevet <join_tevet@protonmail.com>
2021-06-04 21:02:54 +0700

    Add github workflows

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -227,7 +227,7 @@ export default function Footer(props) {
             </div>
             <div className={classes.text}>
               DefiFarms &copy; {1900 + new Date().getYear()} All Rights Reserved
-              | Power by <span className={classes.icon} /> Binance Smart Chainn
+              | Power by <span className={classes.icon} /> Binance Smart Chainnn
             </div>
           </div>
         </footer>
